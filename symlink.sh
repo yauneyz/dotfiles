f96612635eef397a5f98f74390e583865766c25b
Zac Yauney <zac.yauney@gmail.com>
2020-09-01 11:51:50 -0600

    Fix sily tmux typo

--- symlink.sh
+++ symlink.sh
@@ -5,7 +5,7 @@ ln -sv ~/dotfiles/.i3/ ~
 ln -sv ~/dotfiles/..oh-my-zsh ~
 ln -sv ~/dotfiles/.oh-my-zsh ~
 ln -sv ~/dotfiles/.p10k.zsh ~
-ln -sv ~/dotfiles/.tmux.comf ~
+ln -sv ~/dotfiles/.tmux.conf ~
 ln -sv ~/dotfiles/.vimrc ~
 ln -sv ~/dotfiles/.zshrc ~
 ln -sv ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
