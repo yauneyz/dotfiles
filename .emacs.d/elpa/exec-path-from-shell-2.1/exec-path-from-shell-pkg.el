a42643fe6b2140f68dacde43fdd0ba056e5e4e1d
Zac Yauney <zac.yauney@gmail.com>
2024-11-21 19:07:58 -0800

    Delete stuff

--- .emacs.d/elpa/exec-path-from-shell-2.1/exec-path-from-shell-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from exec-path-from-shell.el  -*- no-byte-compile: t -*-
-(define-package "exec-path-from-shell" "2.1" "Get environment variables such as $PATH from the shell" '((emacs "24.1") (cl-lib "0.6")) :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :keywords '("unix" "environment") :url "https://github.com/purcell/exec-path-from-shell")
